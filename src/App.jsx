2979fe88a1ddbf48c2f42e331a8ee585de793891
Jordan Youngs <jordankyoungs@gmail.com>
2018-09-05 23:14:02 +0000

    State changes are now sent to the server and broadcasted to all clients

--- src/App.jsx
+++ src/App.jsx
@@ -3,36 +3,26 @@ import NavBar from './NavBar.jsx';
 import MessageList from './MessageList.jsx';
 import ChatBar from './ChatBar.jsx';
 
-let increasingId = 3;
-
-const dummyData = {
-  currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
-  messages: [
-    {
-      id: "1",
-      type: 'message',
-      username: "Bob",
-      content: "Has anyone seen my marbles?",
-    },
-    {
-      id: "2",
-      type: 'message',
-      username: "Anonymous",
-      content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
-    }
-  ]
-}
-
 class App extends Component {
   constructor() {
     super();
-    this.state = dummyData;
+    this.state = {
+      currentUser: {name: 'Bob'},
+      messages: []
+    };
   }
 
   componentDidMount() {
     const socket = new WebSocket("ws://localhost:3001");
-    console.log('Connected to server');
     this.setState({ socket });
+
+    socket.onmessage = event => {
+      const messageArr = JSON.parse(event.data);
+      console.log(messageArr);
+      this.setState({
+        messages: this.state.messages.concat(messageArr)
+      });
+    };
   }
 
   render() {
@@ -52,46 +42,34 @@ class App extends Component {
 
   handleMessage = content => {
     const newMessage = {
-      id: increasingId++,
       type: 'message',
       username: this.state.currentUser.name,
       content
     }
-    this.state.socket.send(JSON.stringify(newMessage));
-    const messages = [...this.state.messages, newMessage];
-    this.setState({ messages });
+    this.state.socket.send(JSON.stringify([newMessage]));
   }
 
   handleNameChange = username => {
-    const newMessage = {
-      id: increasingId++,
+    const newNotification = {
       type: 'notification',
       content: `${this.state.currentUser.name} changed their name to ${username}`
     }
-    const messages = [...this.state.messages, newMessage];
-    this.setState({
-      messages,
-      currentUser: {name: username}
-    });
+    this.setState({currentUser: {name: username}});
+    this.state.socket.send(JSON.stringify([newNotification]));
   }
 
   handleBothChange = (username, content) => {
     const newNotification = {
-      id: increasingId++,
       type: 'notification',
       content: `${this.state.currentUser.name} changed their name to ${username}`
     }
+    this.setState({currentUser: {name: username}});
     const newMessage = {
-      id: increasingId++,
       type: 'message',
       username: username,
       content
     }
-    const messages = [...this.state.messages, newNotification, newMessage];
-    this.setState({
-      messages,
-      currentUser: {name: username}
-    });
+    this.state.socket.send(JSON.stringify([newNotification, newMessage]));
   }
 }
 export default App;
